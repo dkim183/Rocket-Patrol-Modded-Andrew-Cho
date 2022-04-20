6ddc7f970dc538bb3ba03feab59bc0593f43aaee
Andrew <dkim183@ucsc.edu>
2022-04-20 02:19:35 -0700

    Final Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,17 +5,23 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.image('menuscreen', './assets/starfield.png'); // Home-made Original background
+        this.load.audio('sfx_select', './assets/sorry.wav');  // Voice made within https://15.ai
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
-        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.audio('sfx_rocket', './assets/p1Shoot.wav');  // Home made with audacity
         this.load.audio('sfx_menu_music', './assets/menu_music.wav') //Copy-right Free Music: Epic Cinematic Gaming Cyberpunk | RESET by Alex-Productions
     }
 
     create() {
+
+        // place menu background
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'menuscreen').setOrigin(0, 0);
+
+
         // menu text configuration
         let menuConfig = {
             fontFamily: 'Arial',
-            fontSize: '30px',
+            fontSize: '20px',
             backgroundColor: '#ff0000',
             color: '#ffffff',
             align: 'right',
@@ -29,14 +35,14 @@ class Menu extends Phaser.Scene {
 
         // show menu text
 
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize*4 - borderPadding*4, 'The Extreme Multiplayer Experience', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 - borderUISize*4 - borderPadding*4, 'The Most Extreme Multiplayer Experience!!?', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#03ffff';
         menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2, 'P1: Use ←→ arrows to move & (L) to fire', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'P2: Use A + D to move & (G) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*3 + borderPadding*3, 'P1: Use ←→ arrows to move & (L) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*4 + borderPadding*4, 'P2: Use A + D to move & (G) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*4 + borderPadding*4, 'Press ← for Easy or → for HARD', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize*5 + borderPadding*5, 'Press ← for Easy or → for HARD', menuConfig).setOrigin(0.5);
 
         // menu Music = stops the music once a left or right button is pressed.
         this.menuMusic = this.sound.add('sfx_menu_music');
